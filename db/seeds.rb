6210e0983c2535c02d91daf177bee4018676e4fc
Ross Wilson <me@rosswilson.co.uk>
2020-12-23 23:34:39 +0000

    Regenerated ActionText and ActiveStorage migrations and related config

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Make hashing passwords faster during local development
+BCrypt::Engine.cost = 4
+
 120.times do |index|
   User.create!(name: "User #{index}", email: "user-#{index}@example.com", password: "password")
 end
